4154df4d91baee866ba0122794b9e93843ee5d0b
aren125 <ren24007@byui.edu>
2026-09-22 18:18:53 -0600

    Create Hello-World
    Hello world displayed as typewriter

---
+++ hello-world.py
@@ -0,0 +1,12 @@
+import sys 
+import time
+
+def type_writer(text, delay=0.05):
+    for char in text:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(delay)
+    print()  # Move to the next line
+
+type_writer("Hello, World!")
+
